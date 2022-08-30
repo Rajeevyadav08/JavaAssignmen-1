5a36938190a538f5d1639e12cf3a245f14234dbc
Rajeev Yadav <103309049+Rajeevyadav08@users.noreply.github.com>
2022-08-30 12:03:46 +0530

    Add files via upload

---
+++ parasq.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class parasq {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int x,parameter;
+        System.out.print("Enter the side of square: ");
+        x=sc.nextInt();
+        parameter=4*x;
+        System.out.println(parameter);
+    }
+    
+}
+
