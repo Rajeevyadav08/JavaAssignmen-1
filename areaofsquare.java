f13ca1c0706c9cad9e40e947e9286e31286e895d
Rajeev Yadav <103309049+Rajeevyadav08@users.noreply.github.com>
2022-08-30 12:00:09 +0530

    Add files via upload

---
+++ areaofsquare.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class areaofsquare {
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        int x,area;
+        System.out.print("Enter side of square : ");
+        x=sc.nextInt();
+        area=x*x;
+        System.out.println(area);
+
+    }
+    
+}
